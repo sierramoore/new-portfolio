7e059a57eccac205ae01362bb77c5e14cb5a3881
Sierra Moore <sierra.v.moore@gmail.com>
2020-06-23 02:14:44 +0200

    added resume page from old site, made img slideshow

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-// const folded = new-portfolio OriDomi('.paper',
-//     {
-//         // ripple: true // to stagger effect
-//     });
-// folded.accordion(20).wait(1000).accordion(-20);
-
-
-const elem = document.getElementsByClassName('paper')[0]
-
-const folded = new OriDomi(elem);
-
-elem.addEventListener('mouseover',function(){
-    folded.accordion(-60, 0);
-},false);
-
-elem.addEventListener('mouseout',function(){
-    folded.reset();
-},false);
-
-
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
@@ -26,11 +6,92 @@ document.addEventListener('mousemove', e => {
 })
 
 
-document.addEventListener('click', () => {
-    cursor.classList.add("cursor-clicked")
+// document.addEventListener('click', () => {
+//     cursor.classList.add("cursor-clicked")
+//
+//     // remove class after .5s
+//     setTimeout(() => {
+//         cursor.classList.remove("cursor-clicked")
+//     }, 500)
+// })
 
-    // remove class after .5s
-    setTimeout(() => {
-        cursor.classList.remove("cursor-clicked")
-    }, 500)
+
+const projectImg = document.getElementById('projectImg');
+const projectTitle = document.getElementById('projectTitle');
+const projectDesc = document.getElementById('projectDesc');
+
+const nextProject = document.getElementById('project-next');
+const prevProject = document.getElementById('project-back');
+
+const projectImgs = [
+    'url("../../img/vest.png")',
+    'url("../../img/harborhq.png")',
+    'url("../../img/perfectionpainting.png")',
+    'url("../../img/ust.png")'
+];
+const projectTitles = [
+    'VestHQ',
+    'HarborHQ',
+    'Perfection Painting',
+    'UST Aviation'
+];
+
+
+const projectDescriptions = [
+    'Created the website for a fin-tech startup to showcase the features of the company.',
+    'Created the website for a fin-tech startup to serve as a client login portal.',
+    'Designed and developed website for a commercial painting company to showcase work and send work inquires.',
+    'Recreated old site from Flash, changed hosting and email routing'
+];
+
+const projects = {
+    one: {
+        img: 'url("../../img/vest.png")',
+        title: 'VestHQ',
+        description: 'Created the website for a fin-tech startup to showcase the features of the company.'
+    },
+    two: {
+        img: 'url("../../img/harborhq.png")',
+        title: 'HarborHQ',
+        description: 'Created the website for a fin-tech startup to serve as a client login portal.'
+    },
+    three: {
+        img: 'url("../../img/perfectionpainting.png")',
+        title: 'Perfection Painting',
+        description: 'Designed and developed website for a commercial painting company to showcase work and send work inquires.'
+    },
+    four: {
+        img: 'url("../../img/ust.png")',
+        title: 'UST Aviation',
+        description: 'Recreated old site from Flash, changed hosting and email routing'
+    }
+}
+
+let bgCount = 0;
+nextProject.addEventListener('click', (e) => {
+    if (bgCount <= 0) { bgCount = 1 }
+    bgCount++;
+    if (bgCount > projectImgs.length - 1) bgCount = 0;
+    projectImg.style.backgroundImage = projectImgs[bgCount];
+    console.log(bgCount, 'next')
+
+    console.log(projectTitles.innerText)
+    projectTitles.innerText = projectTitles[bgCount];
+    projectDescriptions.innerText = projectTitles[bgCount];
 })
+
+prevProject.addEventListener('click', (e) => {
+
+    if (bgCount <= 0) { bgCount = 0 }
+    console.log(bgCount, 'back')
+    projectImg.style.backgroundImage = projectImgs[bgCount - 1];
+    bgCount--;
+
+    projectTitles.innerText = projectTitles[bgCount - 1];
+    projectDescriptions.innerText = projectTitles[bgCount -1];
+
+})
+
+// = projectImgs[]
+
+
---
+++ js/oridomi.js
@@ -0,0 +1,18 @@
+// const folded = new-portfolio OriDomi('.paper',
+//     {
+//         // ripple: true // to stagger effect
+//     });
+// folded.accordion(20).wait(1000).accordion(-20);
+
+
+// const elem = document.getElementsByClassName('paper')[0]
+//
+// const folded = new OriDomi(elem);
+//
+// elem.addEventListener('mouseover',function(){
+//     folded.accordion(-60, 0);
+// },false);
+//
+// elem.addEventListener('mouseout',function(){
+//     folded.reset();
+// },false);
